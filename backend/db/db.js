d702f5e9638b37cfc7e6b36bc0e74aca5a2bbdff
Minami189 <Soon128HD@gmail.com>
2025-02-12 19:47:03 +0800

    fixed env vars

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
-const urlDB = `mysql://${{MYSQLUSER}}:${{MYSQL_ROOT_PASSWORD}}@${{RAILWAY_PRIVATE_DOMAIN}}:3306/${{MYSQL_DATABASE}}`;
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}`;
 const pool = mysql.createPool(urlDB).promise();
 
 
