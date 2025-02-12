16f7fdfaf3fbefa48751d90e8fa555d1ceecbc69
Minami189 <Soon128HD@gmail.com>
2025-02-12 21:35:42 +0800

    changed urlDb used

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,8 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}`;
+
+const urlDB = process.env.RAILWAY_DATABASE_URL;
 const pool = mysql.createPool(urlDB).promise();
 
 
