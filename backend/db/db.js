8b4c9042d19afd8964b7773af2d39bd3b0e1b224
Minami189 <Soon128HD@gmail.com>
2025-02-15 14:08:02 +0800

    fixed db

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const urlDB = {host: process.env.HOST, user: process.env.USER, password: process.env.PASSWORD, database: process.env.DATABASE}//process.env.RAILWAY_DATABASE_URL;
+const urlDB = process.env.RAILWAY_DATABASE_URL;
 const pool = mysql.createPool(urlDB).promise();
 
 export default pool;
